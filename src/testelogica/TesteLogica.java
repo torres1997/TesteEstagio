05ab194adfc9cba6d138bca59b8908f7bb8984a4
Luiz_Torres <torresluizalves@gmail.com>
2021-01-25 23:34:34 -0400

    Segundo Exercicio

--- src/testelogica/TesteLogica.java
+++ src/testelogica/TesteLogica.java
@@ -9,22 +9,34 @@ public class TesteLogica {
 
     public static void main(String[] args) {
         //Exercicio01
-        imprimeNumero();
-  
+        //imprimeNumero();
+        
+        //Exercicio02
+        int[] a = {1,2,3,4,5};
+        
+        System.out.print(somaDeQuadrados(a));
     }
     
     private static void imprimeNumero(){
         
         for(int i = 1; i<=100; i++){
             System.out.printf("\n %d", i);
-            if(i%3==0){
+            if(i%3==0)
                 System.out.printf("BUZZ");
-            }
-            if(i%5==0){
+            if(i%5==0)
                 System.out.printf("BIZZ");
-            }
+        }
+    }
+
+    
+    private static int somaDeQuadrados(int[] a) {
+        int soma = 0;
+        
+        for(int i=0; i<a.length;i++){
+            soma += a[i]*a[i];
         }
         
+        return soma;
     }
     
 }
